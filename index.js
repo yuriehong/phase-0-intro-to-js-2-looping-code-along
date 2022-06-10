cc7ca8c99d88fc49fe33b0b1e88f14b69d08b3e2
+ <+>
2022-06-09 22:09:38 -0400

    commit tree fb0499ea61aca7eda7f6133d7e223b7bd5187eae

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+function writeCards(names, event){
+    const messages = [];
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return messages;
+}
+function  countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+
+}
